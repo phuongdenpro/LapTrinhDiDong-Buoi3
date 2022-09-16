801f9b84ad2062613054b1272108c1d3ce80baf3
phuong <phuongdenpro@gmail.com>
2022-09-16 10:48:33 +0700

    bai2

--- Bai1a/App.js
+++ Bai1a/App.js
@@ -1,93 +1,94 @@
+import { LinearGradient } from "expo-linear-gradient";
 import { StatusBar } from "expo-status-bar";
-import React from 'react'
+import React from "react";
 import { StyleSheet, Text, View, Image, TouchableOpacity } from "react-native";
 
 const App = () => {
   return (
     <View style={styles.container}>
-      <View style={styles.image}>
-        <Image source={require("./images/circle.png")}></Image>
-      </View>
-      <View style={styles.text}>
-        <View style={styles.text1}>
-          <Text>GROW YOUR BUSINESS</Text>
+      <LinearGradient colors={["#C9EFEF", "#2BF4AE"]} start={[0.9, 0.1]}>
+        <View style={styles.image}>
+          <Image source={require("./images/circle.png")}></Image>
         </View>
-        <View style={styles.text2}>
-          <Text>
-            We will help you to grow your business using online server
-          </Text>
+        <View style={styles.text}>
+          <View style={styles.text1}>
+            <Text>GROW YOUR BUSINESS</Text>
+          </View>
+          <View style={styles.text2}>
+            <Text>
+              We will help you to grow your business using online server
+            </Text>
+          </View>
         </View>
-      </View>
-      <View style={styles.bottom}>
-        <TouchableOpacity style={styles.btnLogin}>
-          <Text>Login</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.btnSign}>
-          <Text>Sign up</Text>
-        </TouchableOpacity>
-      </View>
-      <View style={styles.bottomText}>
-        <Text>How we work?</Text>
-      </View>
+        <View style={styles.bottom}>
+          <TouchableOpacity style={styles.btnLogin}>
+            <Text>Login</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.btnSign}>
+            <Text>Sign up</Text>
+          </TouchableOpacity>
+        </View>
+        <View style={styles.bottomText}>
+          <Text>How we work?</Text>
+        </View>
+      </LinearGradient>
     </View>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#2BF4AE",
+  //  backgroundColor: "#2BF4AE",
     alignItems: "center",
     justifyContent: "center",
   },
-  image:{
-    flex:2,
+  image: {
+    flex: 2,
     alignItems: "center",
     justifyContent: "center",
   },
-  text:{
-    flex:1,
-  },
-  text1:{
-    flex:1,
-    alignItems:'center',
-    fontSize:25,
-    fontWeight:'bold',
-    color:'black',
-    
-  },
-  text2:{
+  text: {
     flex: 1,
-    marginHorizontal:20,
-    textAlign:'center',
-    
   },
-  bottom:{
+  text1: {
     flex: 1,
-    flexDirection:"row"
+    alignItems: "center",
+    fontSize: 40,
+    fontWeight: "bold",
+    color: 'black',
   },
-  btnLogin:{
-    flex:1,
-    backgroundColor:'#E3C000',
-    height:50,
-    marginHorizontal:30,
-    justifyContent:'center',
-    alignItems:'center',
+  text2: {
+    flex: 1,
+    marginHorizontal: 20,
+    textAlign: "center",
   },
-  btnSign:{
-    flex:1,
-    backgroundColor:'#E3C000',
-    height:50,
-    marginHorizontal:30,
-    justifyContent:'center',
-    alignItems:'center'
+  bottom: {
+    flex: 1,
+    flexDirection: "row",
+  },
+  btnLogin: {
+    flex: 1,
+    backgroundColor: "#E3C000",
+    height: 50,
+    marginHorizontal: 30,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  btnSign: {
+    flex: 1,
+    backgroundColor: "#E3C000",
+    height: 50,
+    marginHorizontal: 30,
+    justifyContent: "center",
+    alignItems: "center",
   },
 
-  bottomText:{
-    flex:1,
-    fontSize:30,
-    fontWeight:'700'
-  }
-
+  bottomText: {
+    flex: 1,
+    fontSize: 30,
+    fontWeight: "700",
+    alignItems: "center",
+  },
 });
 export default App;
