2e1b8e8348db877e47a303943cf554dc4efd1678
phuong <phuongdenpro@gmail.com>
2022-09-16 09:50:35 +0700

    bai1

--- Bai1a/App.js
+++ Bai1a/App.js
@@ -1,11 +1,30 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Image, TouchableOpacity } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View style={styles.image}>
+        <Image source={require("./images/circle.png")}></Image>
+      </View>
+      <View style={styles.text}>
+        <View style={styles.text1}>
+          <Text>GROW YOUR BUSINESS</Text>
+        </View>
+        <View style={styles.text2}>
+          <Text>
+            We will help you to grow your business using online server
+          </Text>
+        </View>
+      </View>
+      <View style={styles.bottom}>
+        <TouchableOpacity style={styles.btnLogin}>
+          <Text>Login</Text>
+        </TouchableOpacity>
+        <TouchableOpacity style={styles.btnSigin}>
+          <Text>Sign up</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
@@ -13,8 +32,9 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
+  
 });
